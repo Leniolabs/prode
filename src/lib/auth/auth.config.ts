b71018d082de61ddba40f701b397f8b4b633449a
Eugenio Vazquez <eugenio.vazquez@improving.com>
2026-06-03 19:35:41 -0300

    Auth, DB and Polishing

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -4,24 +4,48 @@ import Facebook from 'next-auth/providers/facebook'
 import GitHub from 'next-auth/providers/github'
 import Twitter from 'next-auth/providers/twitter'
 
-export const authConfig: NextAuthConfig = {
-  providers: [
+const hasCredentials = (id?: string, secret?: string): id is string =>
+  Boolean(id && secret)
+
+const oauthProviders = []
+
+if (hasCredentials(process.env.GOOGLE_ID, process.env.GOOGLE_SECRET)) {
+  oauthProviders.push(
     Google({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
+  )
+}
+
+if (hasCredentials(process.env.FACEBOOK_ID, process.env.FACEBOOK_SECRET)) {
+  oauthProviders.push(
     Facebook({
-      clientId: process.env.FACEBOOK_ID!,
-      clientSecret: process.env.FACEBOOK_SECRET!,
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
+  )
+}
+
+if (hasCredentials(process.env.GITHUB_ID, process.env.GITHUB_SECRET)) {
+  oauthProviders.push(
     GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
+  )
+}
+
+if (hasCredentials(process.env.TWITTER_ID, process.env.TWITTER_SECRET)) {
+  oauthProviders.push(
     Twitter({
-      clientId: process.env.TWITTER_ID!,
-      clientSecret: process.env.TWITTER_SECRET!,
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
     }),
-  ],
+  )
+}
+
+export const authConfig: NextAuthConfig = {
+  providers: oauthProviders,
   pages: { signIn: '/' },
 }
